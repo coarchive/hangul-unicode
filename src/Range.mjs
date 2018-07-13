c79328ead5b24d8cd71ba9ca9ae2b45778b3ee74
Aoide <arcanehood@gmail.com>
2018-07-12 19:32:17 -0700

    SAVE the world

---
+++ src/Range.mjs
@@ -0,0 +1,13 @@
+export default class Range {
+  constructor(start, end) {
+    if (typeof (start + end) !== 'number') {
+      throw new Error('Both arguments to the Range constructor must be numbers!');
+    }
+    this.start = start;
+    this.end = end;
+  }
+
+  contains(num) {
+    return num >= this.start && num <= this.end;
+  }
+}
---
+++ src/transformer.mjs
@@ -0,0 +1,1 @@
+// transforms jamo into compatibility jamo
