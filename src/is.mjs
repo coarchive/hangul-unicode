d84e5feb14002b8b5e21b11c09c370a6d433c6da
Aoide <arcanehood@gmail.com>
2018-07-15 12:49:19 -0700

    Swapped if-statement order for slightly faster failing

--- src/is.mjs
+++ src/is.mjs
@@ -3,10 +3,10 @@ import assertChar from './assertChar';
 
 export default (range => (char) => {
   assertChar(char);
-  if (Array.isArray(range)) {
+  if (range instanceof Range) {
+    return range.contains(char.codePointAt(0));
+  } if (Array.isArray(range)) {
     return range.includes(char);
-  } if (range instanceof Range) {
-    return range.contains(char.charCodeAt(0));
   } if (typeof range === 'object') {
     return !!range[char];
   }
