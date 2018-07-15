24cf9dffa4f2fbf209bd51bd6ed764b6e3634985
Aoide <arcanehood@gmail.com>
2018-07-15 13:04:21 -0700

    I need to learn how to spell correctly

--- src/composeComplex.mjs
+++ src/composeComplex.mjs
@@ -1,5 +1,16 @@
-import * as cx from './unicode/complex';
+import { complex, irregularComplex } from './unicode/complex';
 
-export default ((..chars) => {
-
-});
+const descend = o => (...ary) => {
+  const lowerObject = o[ary[0]];
+  if (lowerObject) {
+    return descend(lowerObject, ary.slice(1));
+  }
+  return false;
+};
+const composeComplex = descend(complex);
+export const composeIrregularComplex = descend(irregularComplex);
+export default (composeComplex);
+export const composeAnyComplex = (...ary) => (
+  composeComplex(...ary)
+  || composeIrregularComplex(...ary)
+);
